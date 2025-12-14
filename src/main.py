7c349c35edf172d86b2fd1601e5a3391079eb28e
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2025-12-14 00:32:59 -0500

    update: initialize the application

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from photo-portfolio!")
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi import templating
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from contextlib import asynccontextmanager
+
+from src.models.models import Photo, Comment, User
+from src.database import init_db, get_db
+from src.config import DBConfig, EnvType
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    config = DBConfig.from_env()
+    if config.env_type == EnvType.DEVELOPMENT:
+        init_db()
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
