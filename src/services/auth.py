69eec9f82a1a2ab67ac2deb5e32e116b39bcb1ca
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2025-12-29 12:31:11 -0500

    update: implement helper functions for user authentication

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,2 +1,18 @@
 import jwt
 from pwdlib import PasswordHash
+
+password_hash: PasswordHash = PasswordHash.recommended()
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """check to see if password entered matches the hash"""
+    return password_hash.verify(password=plain_password, hash=hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    """generate a hashed password"""
+    return password_hash.hash(password=password)
+
+
+def authenticate_user(db: Session, username: str, password: str) -> User | bool:
+    """authenticates the user against the db"""
